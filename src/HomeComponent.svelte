0541ef6edbbf97fd997f6aac712f3e485efc4424
Soleod <hojnacki.bartosz@gmail.com>
2023-03-29 00:34:37 +0200

    Restructurization

---
+++ src/HomeComponent.svelte
@@ -0,0 +1,31 @@
+<script>
+    import {Button, Column, Content, Grid, ImageLoader, Row} from "carbon-components-svelte";
+</script>
+
+
+<Content>
+    <Grid>
+        <Row>
+            <Column>
+                <h1>Hedgehog Burrow</h1>
+                <ImageLoader
+                        src="https://img.itch.zone/aW1nLzEwMTAwMDc1LnBuZw==/original/cdR%2FmU.png"
+                />
+                <br>
+                <br>
+                <br>
+                <br>
+                <h2>
+                    Build your own hedgehog burrow to help the little guys survive the winter!
+                    Your goal in this game is to have as many hedgehogs as you can after 96 days.
+                    To archive this goal you need to take care of food and sleeping needs of your little friends.
+                </h2>
+                <br>
+                <br>
+                <br>
+                <br>
+                <Button href="/HedgehogGame.html">Play</Button>
+            </Column>
+        </Row>
+    </Grid>
+</Content>
